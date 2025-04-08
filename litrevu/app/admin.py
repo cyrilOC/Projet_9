20e9f37cf22464d0b952808cdfcd666d383ec070
cyrilOC <cyril@vu-du.net>
2025-04-08 16:58:31 +0200

    Register UserFollows model in admin with custom admin interface

--- litrevu/app/admin.py
+++ litrevu/app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Ticket, Review
+from .models import Ticket, Review, UserFollows
 
 admin.site.register(Ticket)
 admin.site.register(Review)
+
+@admin.register(UserFollows)
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user', 'follow_date',)
 
 # Register your models here.
